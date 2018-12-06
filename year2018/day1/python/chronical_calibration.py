d5853ddbe6dbebf0f8bea14319fa4dd608e231b1
rphu <rphu26@gmail.com>
2018-12-05 19:44:25 -0700

    add functions for completing repeat problem

--- year2018/day1/python/chronical_calibration.py
+++ year2018/day1/python/chronical_calibration.py
@@ -2,5 +2,25 @@ def calibrate(cal_file, start_freq=0):
     with open(cal_file, 'r') as f:
         return sum(int(line) for line in f) + start_freq
 
+def first_repeat_output(cal_file, start_freq=0):
+    found_freq = [start_freq]
+    final_freq = start_freq
+    for i in _generate_repeat_input(cal_file):
+        final_freq += i
+        if final_freq not in found_freq:
+            found_freq.append(final_freq)
+        else:
+            return final_freq
+
+
+def _generate_repeat_input(cal_file):
+    with open(cal_file, 'r') as f:
+        vals = [int(line) for line in f]
+    while True:
+        for i in vals:
+            yield i
+
+
 if __name__ == '__main__':
-    print(calibrate('input.txt', 0))
+    print(f'Initial calibration: {calibrate("input.txt", 0)}')
+    print(f'First repeated frequency: {first_repeat_output("input.txt", 0)}')
